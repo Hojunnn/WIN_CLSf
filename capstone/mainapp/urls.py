44133776107c594f29a9b822053ae979384eabc1
Hojunnn <tjdghwns1792@naver.com>
2020-11-18 17:03:33 +0900

    remove useless

--- capstone/mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib import admin
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('new/', views.new , name='new'),
-]
