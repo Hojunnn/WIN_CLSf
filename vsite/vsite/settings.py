29e3d12a42cfcc9ff6164e9b89c22ca31ebf74ff
br1795 <52655291+br1795@users.noreply.github.com>
2020-12-01 04:28:31 +0900

    analysis

--- vsite/vsite/settings.py
+++ vsite/vsite/settings.py
@@ -130,3 +130,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MODEL_ROOT =os.path.join(BASE_DIR,'models')
--- vsite/vsite/urls.py
+++ vsite/vsite/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('upload.urls'), name='uplaod'),
+    url(r'^analysis/', include('analysis.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
